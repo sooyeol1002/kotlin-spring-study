dcac591de84fb4c157151c8de882e481ba4dd1e7
sooyeol1002 <didtnduf2000@gmail.com>
2023-09-11 17:50:58 +0900

    추가

--- src/main/kotlin/com/example/myapp/configuration/WebMvcConfig.kt
+++ src/main/kotlin/com/example/myapp/configuration/WebMvcConfig.kt
@@ -1,6 +1,6 @@
 package com.example.myapp.configuration
+
 import com.example.myapp.auth.AuthInterceptor
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
